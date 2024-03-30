f35216f9145589e73e413e83992142cb6e8d5a7a
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-03-30 18:20:18 +0000

    update dev dependencies so project runs
    add code to render info from data.js and make it clickable

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,15 +1,34 @@
 // single selection accordian
 // multiple selection accordian
 
+import { useState } from "react";
+import data from "./data";
+import "./styles.css";
+
 export default function Accordian() {
 
   const [selected, setSelected] = useState(null);
+  
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
 
   return (
     <div className="wrapper">
       <div className="accordian">
         {/* checks to make sure data is being received */}
         {data && data.length > 0 ? 
+        data.map(item => <div className="item"> 
+          <div onClick={() => handleSingleSelection(item.id)} className="title">
+            <h3>{item.question}</h3>
+            <span>+</span>
+          </div>
+          {
+            selected === item.id ? 
+            <div className="">{item.answer}</div>
+            : null
+          }
+        </div>)
         : <div>No data found</div> }
       </div>
     </div>
